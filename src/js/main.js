227b9cfa9a045e6314584bde36562fb33ceaac06
Adam H <herrmann.adamm@gmail.com>
2026-09-19 18:27:18 -0600

    NPS 1 :Add global navbar and hero banner styles; update main.js for dynamic content

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+function parkInfoTemplate(info) {
+  return `<a href="#" class="hero-banner__title">${info.name}</a>
+  <p class="hero-banner__subtitle">
+    <span>${info.designation}</span>
+    <span>${info.states}</span>
+  </p>`;
+}
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.textContent = parkData.fullName;
+
+document.querySelector("head > title").textContent = parkData.fullName;
+const heroImage = document.querySelector(".hero-banner > img");
+heroImage.src = parkData.images[0].url;
+heroImage.alt = parkData.images[0].altText;
+document.querySelector(".hero-banner__content").innerHTML = parkInfoTemplate(parkData);
